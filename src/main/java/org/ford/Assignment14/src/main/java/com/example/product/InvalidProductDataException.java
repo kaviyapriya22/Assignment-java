ad57f01384df1ad97414c4c42521d6c3a17d9995
Kaviyapriya K <kaviyapriyak22@gmail.com>
2025-09-08 20:57:32 +0530

    Added Assignment13 folder

---
+++ src/main/java/org/ford/Assignment14/src/main/java/com/example/product/InvalidProductDataException.java
@@ -0,0 +1,8 @@
+package com.example.product;
+
+public class InvalidProductDataException extends RuntimeException {
+    public InvalidProductDataException(String message) {
+
+        super(message);
+    }
+}
